5ab7993cdbb3f94fb730527a7653dc148eaed182
knikkits <nikki_kheifets@hotmail.com>
2023-01-04 12:24:02 -0800

    server.js file

--- pages/server.js
+++ pages/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const payload = require("payload");
 
 const app = express();
+
+payload.init({
+  secret: "SECRET_KEY",
+  mongoURL: "mongodb://localhost/payload",
+  express: app,
+});
 
 app.listen(3000, async () => {
   console.log(
